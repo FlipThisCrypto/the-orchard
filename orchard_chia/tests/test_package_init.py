7d60814d55dc75d2e08c39d28b3471a86c517953
FlipThisCrypto <flipthiscrypto@gmail.com>
2026-07-21 17:45:02 -0400

    iteration 49: package import surface test
    Verify orchard_chia.datalayer exports SCHEMA_VERSION.

---
+++ orchard_chia/tests/test_package_init.py
@@ -0,0 +1,6 @@
+﻿# SPDX-License-Identifier: Apache-2.0
+"""Sanity: import surface for orchard_chia.datalayer."""
+import orchard_chia.datalayer as dl
+
+def test_package_schema_version():
+    assert dl.SCHEMA_VERSION == "1.0.0"
